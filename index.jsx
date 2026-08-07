3c481d1ba1f0628fa5ebdc174477be09e9ac0377
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-08-07 19:43:02 +0100

    feat: Add vans page

--- index.jsx
+++ index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
 
 import "./server";
 
@@ -15,11 +16,13 @@ function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
