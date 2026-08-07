f119285a2a8910834b27187c22e0cae865011d5f
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-08-07 19:54:32 +0100

    feat: Pull van data from MirageJS local server

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Vans() {
+  React.useEffect(() => {
+    const controller = new AbortController();
+
+    fetch("/api/vans")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Vans fetch request failed.");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+
+    return () => controller.abort();
+  }, []);
+
   return <h1>Vans page goes here 🚐</h1>;
 }
