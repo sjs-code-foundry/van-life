4eea9a6f557a86a2e90b1bbd23f6b9bb9621555c
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-08-14 19:52:16 +0100

    style: Setup grid for van listings

--- components/Van.jsx
+++ components/Van.jsx
@@ -1,13 +1,15 @@
 export default function Van(props) {
   return (
     <div className="van">
-      <img className="van-img" />
-      <p className="van-name">{props.name}</p>
-      <div className="van-rate-box">
+      <img className="van-img" src={props.imgUrl} />
+      <div className="van-name-box">
+        <p className="van-name">{props.name}</p>
         <p className="van-rate">${props.rate}</p>
-        <p>/day</p>
       </div>
-      <p className="van-type">{props.type}</p>
+      <div className="van-footer">
+        <p className="van-rate-unit">/day</p>
+        <p className="van-type">{props.type}</p>
+      </div>
     </div>
   );
 }
