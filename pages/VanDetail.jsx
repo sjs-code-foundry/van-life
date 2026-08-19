7e1ff2570c305e49a4f544a01b8cdd7616d6b027
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-08-19 18:43:05 +0100

    feat: Fetch van details

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -5,5 +5,23 @@ export default function VanDetail() {
   const params = useParams();
   console.log(params);
 
+  const [details, setDetails] = React.useState(null);
+  console.log(details);
+
+  React.useEffect(() => {
+    const controller = new AbortController();
+
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Van details fetch request failed.");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setDetails(data);
+      });
+  }, [params.id]);
+
   return <h1>Van detail page goes here.</h1>;
 }
