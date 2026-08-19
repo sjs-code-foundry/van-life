47a7b5f537cd06c8e31fa42ba70e0e0229433821
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-08-19 17:52:27 +0100

    fix: Improve van listing a11y
    Ensure screen readers correctly describe the various vans.

--- components/Van.jsx
+++ components/Van.jsx
@@ -3,8 +3,15 @@ import { Link } from "react-router-dom";
 export default function Van(props) {
   return (
     <div className="van">
-      <Link to={`/vans/${props.id}`}>
-        <img className="van-img" src={props.imgUrl} />
+      <Link
+        to={`/vans/${props.id}`}
+        aria-label={`View details for ${props.name}, priced at ${props.rate} per day.`}
+      >
+        <img
+          className="van-img"
+          src={props.imgUrl}
+          alt={`Image of ${props.name}`}
+        />
         <div className="van-name-box">
           <p className="van-name">{props.name}</p>
           <p className="van-rate">${props.rate}</p>
