305f29c2ddad4c2d234a4ad6018c797bd4fdb445
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-09-01 19:39:54 +0100

    fix: Add link to header for host page

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,6 +9,7 @@ export default function Header() {
       <nav>
         <Link to="/about">About</Link>
         <Link to="/vans">Vans</Link>
+        <Link to="/host">Host</Link>
       </nav>
     </header>
   );
