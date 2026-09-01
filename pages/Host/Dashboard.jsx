c4607625ffcd663708f1c577bd156494da32cc45
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-09-01 19:48:13 +0100

    feat: Use Outlet to make income and reviews components accessible

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,3 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 export default function Dashboard() {
-  return <h1>Host Dashboard here.</h1>;
+  return (
+    <>
+      <h1>Host Dashboard here.</h1>
+      <Outlet />
+    </>
+  );
 }
