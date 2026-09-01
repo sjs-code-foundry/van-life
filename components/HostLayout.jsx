5a0f924ab3c53581faa4f3f13a6983d3d10a80b8
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-09-01 20:10:13 +0100

    fix: Remove redundant header component from HostLayout

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -1,16 +1,13 @@
-import { Link } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function HostLayout() {
   return (
     <>
-      <header>
-        <nav>
-          <Link to="/host">Dashboard</Link>
-          <Link to="/host/income">Income</Link>
-          <Link to="/host/reviews">Reviews</Link>
-        </nav>
-      </header>
+      <nav className="host-nav">
+        <Link to="/host">Dashboard</Link>
+        <Link to="/host/income">Income</Link>
+        <Link to="/host/reviews">Reviews</Link>
+      </nav>
       <Outlet />
     </>
   );
