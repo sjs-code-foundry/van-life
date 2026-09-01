562241a65defd41416faf028e5d49df87ff510d5
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-09-01 19:36:08 +0100

    feat: Add host pages

--- index.jsx
+++ index.jsx
@@ -7,6 +7,9 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans/Vans";
 import VanDetail from "./pages/Vans/VanDetail";
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
 
 import "./server";
 
@@ -19,6 +22,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
         </Route>
       </Routes>
     </BrowserRouter>
