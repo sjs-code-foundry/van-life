44d3b397c4e7d65571809d51f443637e486e02ca
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-09-02 19:21:48 +0100

    feat: Change React routes to relative routes

--- index.jsx
+++ index.jsx
@@ -20,10 +20,11 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<HostLayout />}>
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="host" element={<HostLayout />}>
+            {/* <Route path="host" element={<Dashboard />} />*/}
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
