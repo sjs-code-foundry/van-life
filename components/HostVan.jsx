695c5174153e8b086a01a392be2b78fa4906fd5c
sjs-code-foundry <104642983+sjs-code-foundry@users.noreply.github.com>
2026-09-09 19:46:20 +0100

    feat: Implement links to host van detail pages
    Detail pages are still stubs, so I'll build those out next.

--- components/HostVan.jsx
+++ components/HostVan.jsx
@@ -3,14 +3,19 @@ import { Link } from "react-router-dom";
 export default function HostVan(props) {
   return (
     <div className="host-van">
-      <img
-        src={props.imgUrl}
-        alt={`Image of van ${props.id}, called ${props.name}.`}
-      />
-      <div className="host-van-name-area">
-        <p>{props.name}</p>
-        <p className="host-van-name-area-rate">${props.rate}/day</p>
-      </div>
+      <Link
+        to={`/host/vans/${props.id}`}
+        aria-label={`View host-specific details for ${props.name}, priced at ${props.rate} per day.`}
+      >
+        <img
+          src={props.imgUrl}
+          alt={`Image of van ${props.id}, called ${props.name}.`}
+        />
+        <div className="host-van-name-area">
+          <p>{props.name}</p>
+          <p className="host-van-name-area-rate">${props.rate}/day</p>
+        </div>
+      </Link>
     </div>
   );
 }
